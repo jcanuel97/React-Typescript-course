17c20864060d7bcc0abc7c1e3895bbebc7385325
jcanuel <julien.canuel.1@ens.etsmtl.ca>
2021-05-16 11:28:59 -0400

    Have a store for acces to state of Repositories

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypesSelector";
+import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FunctionComponent = () => {
+    //hooks intialization
+    const [term, setTerm] = useState('');
+    const { searchRepositories } = useActions();
+    const { data, error, loading } = useTypedSelector((state) => state.repositories); //Know what we have in redux store
+
+
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        searchRepositories(term);
+    }
+
     return (
         <div>
-            <form>
-                <input></input>
+            <form onSubmit={onSubmit}>
+                <input value={term} onChange={e => setTerm(e.target.value)}></input>
                 <button>Search</button>
             </form>
         </div>
